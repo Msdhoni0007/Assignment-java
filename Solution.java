14e0c3b77e87def89156a5cf476e0834e93b8183
Msdhoni0007 <87920705+Msdhoni0007@users.noreply.github.com>
2022-04-04 10:12:57 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,35 +1,46 @@
-import java.util.*;
-public class Solution
-{public String remove(String s)
-{String res=s;
-String temp="";
- while(temp.length()!=res.length())
-{temp=res;
- res=solve(res);
-}
-return res;
-}
-public String solve(String s)
-{int i=0,n=s.length();
-String ans="";
-while(i<n)
-{if(i<n-1 && s.charAt(i)==s.charAt(i+1))
-{while(i<n-1 &&s.charAt(i)==s.charAt(i+1))
-++i;
-}
-else
-   ans+=s.charAt(i);
-i++;
-}
-return ans;
-}
-public static void main(String[] args)
-{String s;
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the string");
-s=sc.next();
- Solution c =new Solution();
-String s1=c.remove(s);
-System.out.println("String after removing adjacent duplicates :"+s1);
-}
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String removeDuplicates(String str,String temp,int index){
+
+         if(str.equals(""))
+             return "String is empty";
+         if(str.length() == 1)
+             return str;//a
+         if(index < 0)
+             return temp;//a
+
+
+        if(index == str.length()-1 && str.charAt(index) != str.charAt(index-1))
+            temp =str.charAt(index)+temp;//b
+        else if(index >= 1 && str.charAt(index) != str.charAt(index-1) && str.charAt(index) != str.charAt(index+1))
+            temp = str.charAt(index)+temp;
+        else if(index == 0){
+            if(str.charAt(index) != str.charAt(index+1))
+                temp = str.charAt(index)+temp;//temp=a
+        }
+
+
+        return removeDuplicates(str,temp,index-1);//r(abb,"",1),r(abb,"",0),r(abb,a,-1)
+
+     }
+ public static String check(String str,String temp,int i)
+  {
+      String s = "";
+      s = removeDuplicates(str,temp,i);//rem(abb,"",2),rem(a,"",0),rem(a,"",0)
+      if(s.equals(str))
+          return s;//a
+      else 
+           return check(s,"",s.length()-1);//check(a,"",0),check(a,"",0)
+ }
+ public static void main(String[] args)
+    {
+        String str ;
+        String temp= "";
+        Scanner sc =  new Scanner(System.in);
+        str = sc.next();//abb
+        temp = check(str,temp,str.length()-1);//check(abb,"",2)
+        System.out.println(temp);//a
+    }
 }
